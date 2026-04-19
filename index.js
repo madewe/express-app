741b8535b48e9f763cba59f8d2185fe2541881dd
madewe <mademade@duck.com>
2026-04-19 20:56:41 +0200

    docker-compose.yaml added, small change in message

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello my Friend!</h1>')
+  res.send('<h1>Hello Friend!</h1>')
 })
 
 const PORT = 8080
